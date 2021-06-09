edabcb98ae5306b3c2eb6d24a9e25527cc149c30
Nina Davis <nina.k.davis2@gmail.com>
2021-06-09 14:17:22 -0500

    validation

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
   return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username'
+      message: 'Enter your GitHub Username (Required)',
+      validate: githubInput => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log('Please enter your GitHub username!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
     },
     {
       type: 'input',
       name: 'about',
-      message: 'Provide some information about yourself:'
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => confirmAbout
     }
   ]);
 };
+
 const promptProject = portfolioData => {
   console.log(`
 =================
@@ -100,25 +125,15 @@ Add a New Project
       }
     });
 };
+
 promptUser()
-.then(promptProject)
-.then(portfolioData => {
-  console.log(portfolioData);
-  // will be uncommented in lesson 4
-  // const pageHTML = generatePage(portfolioData);
-  // fs.writeFile('./index.html', pageHTML, err => {
-  //   if (err) throw new Error(err);
-  //   console.log('Page created! Check out index.html in this directory to see it!');
-  // });
-});
-  
-//const fs = require('fs');
-//const generatePage = require('./src/page-template');
-
-//const pageHTML = generatePage(name, github);
-
-//fs.writeFile('./index.html', pageHTML, err => {
-  //if (err) throw err;
-
- // console.log('Portfolio complete! Check out index.html to see the output!');
-//});
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+    // will be uncommented in lesson 4
+    // const pageHTML = generatePage(portfolioData);
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
+  });
